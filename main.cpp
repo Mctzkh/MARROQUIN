e986fcf0d737bc3ea999359fb8a4bb49eeefd9d7
Mctzkh <marlonmarro0912@gmail.com>
2025-06-04 18:56:47 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double NumeroUno,NumeroDos;	
+	int opcion;
+	cout<<"***********************************"<<endl;
+	cout<<"*bienvenido a la calculadora de MJ*"<<endl;
+	cout<<"***********************************"<<endl;
+	system("pause");
+	system("cls");
+	
+	do{
+	cout<<"*********************************"<<endl;
+	cout<<"********calculadora de MJ********"<<endl;
+	cout<<"*ingrese que calculo desea hacer*"<<endl;
+	cout<<"*          1.SUMA               *"<<endl;
+	cout<<"*          2.RESTA              *"<<endl;
+	cout<<"*          3.MULTIPLICACION     *"<<endl;
+	cout<<"*          4.DIVICION           *"<<endl;
+	cout<<"*          5.SALIR              *"<<endl;
+	
+	cin>>opcion;
+	}while(opcion!=5);
+	
+	switch(opcion){
+	
+	
+		case 1:
+			cout<<"*********************************"<<endl;
+			cout<<"****ingreso la opcion de suma****"<<endl;
+			
+			
+			
+		break;
+		
+		case 2:
+		break;
+	
+	
+
+	}
+	
+	
+	
+	return 0;
+}
